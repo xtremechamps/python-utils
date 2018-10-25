c26e430a293b990938a4aa6e97f0d03bb87f8ac0
Devendra Dora <dev.tech24@gmail.com>
2018-10-26 01:54:53 +0530

    DatetimeUtil

---
+++ DateTimeUtility.py
@@ -0,0 +1,11 @@
+import sys
+from datetime import timedelta, datetime
+
+if __name__== "__main__":
+   #date in yyyy-MM-dd
+  if len(sys.argv) == 2:
+  	inparg = sys.argv[1]
+  	inpdate = datetime.strptime(inparg, '%Y-%m-%d').date()
+  else:
+  	inpdate = datetime.utcnow() - timedelta(days=1) + timedelta(hours=5,minutes=30)
+  print(str(inpdate))
